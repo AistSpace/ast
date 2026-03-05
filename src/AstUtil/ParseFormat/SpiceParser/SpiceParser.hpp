115a35383a8da456c5dc682b44ae06e0d53ab479
axel <space_ast@163.com>
2026-03-06 00:46:31 +0800

    refactor: reorganize Orientation and add SpiceParser

---
+++ src/AstUtil/ParseFormat/SpiceParser/SpiceParser.hpp
@@ -0,0 +1,42 @@
+///
+/// @file      SpiceParser.hpp
+/// @brief     
+/// @details   
+/// @author    axel
+/// @date      2026-03-05
+/// @copyright 版权所有 (C) 2026-present, ast项目.
+///
+/// ast项目（https://github.com/space-ast/ast）
+/// 本项目基于 Apache 2.0 开源许可证分发。
+/// 您可在遵守许可证条款的前提下使用、修改和分发本软件。
+/// 许可证全文请见：
+/// 
+///    http://www.apache.org/licenses/LICENSE-2.0
+/// 
+/// 重要须知：
+/// 软件按"现有状态"提供，无任何明示或暗示的担保条件。
+/// 除非法律要求或书面同意，作者与贡献者不承担任何责任。
+/// 使用本软件所产生的风险，需由您自行承担。
+
+#pragma once
+
+#include "AstGlobal.h"
+
+AST_NAMESPACE_BEGIN
+
+/*!
+    @addtogroup ParseFormat
+    @{
+*/
+
+/// @brief SPICE内核文件解析器(parser for SPICE kernel file)
+class AST_UTIL_API SpiceParser
+{
+public:
+    SpiceParser() = default;
+    ~SpiceParser() = default;
+};
+
+/*! @} */
+
+AST_NAMESPACE_END
